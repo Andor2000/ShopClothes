ca0da9ab9d0aaee462824a6be0697f19b787f386
Орехов Андрей <aorehov@softrust.ru>
2022-09-25 22:58:31 +0300

    Подсчет итоговой цены в корзине

--- basket.php
+++ basket.php
@@ -8,14 +8,14 @@ include("header.php");
 
 <div class="row">
     <div class="col-md-9 col-sm-12">
-        <div class="row border p-3 mr-2 mb-1" style="background: #ffffff; border-radius: 20px;">
+        <div class="row border p-3 mr-2 mb-1" style="background: #ffffff; border-radius: 20px;" id="backet_tovar_1">
             <div class="col-sm-12 col-md-3 photo_tovar_variant">
                 <img class="img_tovar_variant" src="img/product/asa.jpg" alt="mdo">
             </div>
             <div class="col-sm-12 col-md-9">
                 <div class="d-flex justify-content-between w-100">
                     <h2 class="featurette-heading fw-normal lh mb-0">Zara</h2>
-                    <div class="trash">
+                    <div class="trash" onclick="basket_trash('1')">
                         <img class="w-100" src="img/shop/trash.png" alt="mdo">
                     </div>
                 </div>
@@ -28,27 +28,31 @@ include("header.php");
                     </div>
                     <div class="ml-4">
                         <div>XL</div>
-                        <div>800р</div>
-                        <div>
-                            <button class="btn_count_tovar">-</button>
-                            1
-                            <button class="btn_count_tovar">+</button>
+                        <div class="d-flex">
+                            <div id="basket_price_1">800</div>
+                            <div>р</div>
+                        </div>
+                        <div class="d-flex">
+                            <button class="btn_count_tovar" onclick="minus_count_tovar('1')">-</button>
+                            <div id="basket_count_1">1</div>
+                            <button class="btn_count_tovar" onclick="add_count_tovar('1')">+</button>
                         </div>
                     </div>
                 </div>
-                <div class="mt-2">Итого:
-                    <b>800р</b>
+                <div class="mt-2 d-flex">Итого:
+                    <div class="fw-bolder ml-1 basket_price_tovar" id="backet_itogo_1">800</div>
+                    <div class="fw-bolder">р</div>
                 </div>
             </div>
         </div>
-        <div class="row border p-3 mr-2 mb-1" style="background: #ffffff; border-radius: 20px;">
+        <div class="row border p-3 mr-2 mb-1" style="background: #ffffff; border-radius: 20px;" id="backet_tovar_2">
             <div class="col-sm-12 col-md-3 photo_tovar_variant">
                 <img class="img_tovar_variant" src="img/product/aaa.jpg" alt="mdo">
             </div>
             <div class="col-sm-12 col-md-9">
                 <div class="d-flex justify-content-between w-100">
                     <h2 class="featurette-heading fw-normal lh mb-0">Zara</h2>
-                    <div class="trash">
+                    <div class="trash" onclick="basket_trash('2')">
                         <img class="w-100" src="img/shop/trash.png" alt="mdo">
                     </div>
                 </div>
@@ -61,27 +65,31 @@ include("header.php");
                     </div>
                     <div class="ml-4">
                         <div>XL</div>
-                        <div>800р</div>
-                        <div>
-                            <button class="btn_count_tovar">-</button>
-                            1
-                            <button class="btn_count_tovar">+</button>
+                        <div class="d-flex">
+                            <div id="basket_price_2">800</div>
+                            <div>р</div>
+                        </div>
+                        <div class="d-flex">
+                            <button class="btn_count_tovar" onclick="minus_count_tovar('2')">-</button>
+                            <div id="basket_count_2">1</div>
+                            <button class="btn_count_tovar" onclick="add_count_tovar('2')">+</button>
                         </div>
                     </div>
                 </div>
-                <div class="mt-2">Итого:
-                    <b>800р</b>
+                <div class="mt-2 d-flex">Итого:
+                    <div class="fw-bolder ml-1 basket_price_tovar" id="backet_itogo_2">800</div>
+                    <div class="fw-bolder">р</div>
                 </div>
             </div>
         </div>
-        <div class="row border p-3 mr-2 mb-1" style="background: #ffffff; border-radius: 20px;">
+        <div class="row border p-3 mr-2 mb-1" style="background: #ffffff; border-radius: 20px;" id="backet_tovar_3">
             <div class="col-sm-12 col-md-3 photo_tovar_variant">
                 <img class="img_tovar_variant" src="img/product/asf.jpeg" alt="mdo">
             </div>
             <div class="col-sm-12 col-md-9">
-                <div class="d-flex w-100">
+                <div class="d-flex justify-content-between w-100">
                     <h2 class="featurette-heading fw-normal lh mb-0">Zara</h2>
-                    <div class="trash">
+                    <div class="trash" onclick="basket_trash('3')">
                         <img class="w-100" src="img/shop/trash.png" alt="mdo">
                     </div>
                 </div>
@@ -94,16 +102,20 @@ include("header.php");
                     </div>
                     <div class="ml-4">
                         <div>XL</div>
-                        <div>800р</div>
-                        <div>
-                            <button class="btn_count_tovar">-</button>
-                            1
-                            <button class="btn_count_tovar">+</button>
+                        <div class="d-flex">
+                            <div id="basket_price_3">800</div>
+                            <div>р</div>
+                        </div>
+                        <div class="d-flex">
+                            <button class="btn_count_tovar" onclick="minus_count_tovar('3')">-</button>
+                            <div id="basket_count_3">1</div>
+                            <button class="btn_count_tovar" onclick="add_count_tovar('3')">+</button>
                         </div>
                     </div>
                 </div>
-                <div class="mt-2">Итого:
-                    <b>800р</b>
+                <div class="mt-2 d-flex">Итого:
+                    <div class="fw-bolder ml-1 basket_price_tovar" id="backet_itogo_3">800</div>
+                    <div class="fw-bolder">р</div>
                 </div>
             </div>
         </div>
@@ -114,9 +126,12 @@ include("header.php");
         <h3>Детали заказа</h3>
         <ul style="padding-left: 0;">
             <li>
-                <div class="mr-2">Товары (2)</div>
-                <div></div>
-                <div class="ml-2">800р</div>
+                <div class="mr-2">Товары (3)</div>
+                <div class="mr-2"></div>
+                <div class="d-flex">
+                    <div id="basket_details_order">2 400</div>
+                    р
+                </div>
             </li>
             <li>
                 <div class="mr-2">Скидка</div>
@@ -124,7 +139,12 @@ include("header.php");
                 <div class="ml-2">0р</div>
             </li>
         </ul>
-        <h4 class="mt-4">Итого: 800р</h4>
+        <h4 class="mt-4 d-flex">Итого:
+            <div class="d-flex ml-1">
+                <div id="basket_details_order_itogo">2 400</div>
+                <div>р</div>
+            </div>
+        </h4>
         <div class="mb-3">
             <button class="button_buy w-100" style="height: 55px;">Перейти к оформлению</button>
         </div>
--- js/script.js
+++ js/script.js
@@ -13,10 +13,51 @@ function change_photo_variant(src, number) {
 
 }
 
-function add_count_tovar(data) {
-    var price = Number.prototype.toFixed.call(parseFloat(data) || 0, 2),
-        //заменяем точку на запятую
+
+// 
+// Корзина
+// 
+function add_count_tovar(id_tovar) {
+    let count = Number(document.getElementById('basket_count_' + id_tovar).innerHTML) + 1;
+    let price = Number(document.getElementById('basket_price_' + id_tovar).innerHTML);
+    document.getElementById('basket_count_' + id_tovar).innerHTML = count;
+    document.getElementById('backet_itogo_' + id_tovar).innerHTML = beautiful_price(price * count);
+
+    basket_count_price_itogo();
+}
+
+function minus_count_tovar(id_tovar) {
+    if (Number(document.getElementById('basket_count_' + id_tovar).innerHTML) > 1) {
+        let count = Number(document.getElementById('basket_count_' + id_tovar).innerHTML) - 1;
+        let price = Number(document.getElementById('basket_price_' + id_tovar).innerHTML);
+        document.getElementById('basket_count_' + id_tovar).innerHTML = count;
+        document.getElementById('backet_itogo_' + id_tovar).innerHTML = beautiful_price(price * count);
+
+        basket_count_price_itogo();
+    }
+}
+
+function basket_trash(id_tovar) {
+    document.getElementById('backet_tovar_' + id_tovar).style.display = "none";
+    document.getElementById('backet_itogo_' + id_tovar).innerHTML = 0;
+    basket_count_price_itogo();
+
+}
+
+function basket_count_price_itogo() {
+    let itogo_price = document.getElementsByClassName('basket_price_tovar');
+    let itogo = 0;
+    for (var i = 0; i < itogo_price.length; ++i) {
+        itogo = itogo + Number(itogo_price[i].innerHTML.split(' ').join(''));
+    };
+
+    document.getElementById('basket_details_order').innerHTML = beautiful_price(itogo)
+    document.getElementById('basket_details_order_itogo').innerHTML = beautiful_price(itogo)
+}
+
+// красивый вывод цены
+function beautiful_price(data) {
+    return price = Number.prototype.toFixed.call(parseFloat(data) || 0),
         price_sep = price.replace(/(\D)/g, ","),
-        //добавляем пробел как разделитель в целых
         price_sep = price_sep.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
 }
